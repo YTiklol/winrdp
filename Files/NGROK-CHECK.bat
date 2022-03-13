c840c25292ff9d150894ae3afc0a173cb0fbe121
StrikcYT <60039241+StrikcYT@users.noreply.github.com>
2022-03-14 01:26:21 +0600

    1

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -2,13 +2,13 @@
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"StrikcShop VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user administrator @#Strikc123 /add >nul
-net localgroup administrators administrator /add >nul
+net user WallDev WallDev123 /add >nul
+net localgroup administrators WallDev /add >nul
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
-echo User1: Administrator
-echo Pass: @#Strikc123
+echo User1: WallDev
+echo Pass: WallDev123
 curl -O https://raw.githubusercontent.com/StrikcYT/c9ffin/master/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/StrikcYT/c9ffin/master/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\npp.7.9.4.Installer.x64.exe" https://raw.githubusercontent.com/StrikcYT/c9ffin/master/Files/npp.7.9.4.Installer.x64.exe > out.txt 2>&1
